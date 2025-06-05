f0339d0a4e35d2548d0e05cad6697cba2eb18b61
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-06-05 12:29:11 -0300

    Add tests with jest

---
+++ src/test/Service.test.ts
@@ -0,0 +1,9 @@
+import { getCoolName } from "../app/Service";
+
+describe("Service test suite", () => {
+  test("Name should be cool", () => {
+    const actual = getCoolName();
+    const expected = "CoolName";
+    expect(actual).toBe(expected);
+  });
+});
